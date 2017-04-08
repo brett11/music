6ca882048e6e70edd97e2a61b89fe6215963d3af
Brett F <brettfox11@gmail.com>
2017-04-08 14:42:45 -0500

    added root route and page needed for initial heroku deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+    def heroku
+      render text: "Need this for heroku setup! Wombat!"
+    end
 end
