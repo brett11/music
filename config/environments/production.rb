7db81e17bf9a20ccf02cc0b495f3f1707644ef38
Brett F <brettfox11@gmail.com>
2017-04-18 15:11:47 -0500

    update config for paperclip due to s3 endpoint error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
+      # top config from https://github.com/thoughtbot/paperclip/issues/2151
+      s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
