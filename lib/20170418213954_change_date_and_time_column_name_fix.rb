703978a42af72e315b4b668ff8d248bd2af6fb5d
Brett F <brettfox11@gmail.com>
2017-04-21 14:27:08 -0500

    Album cover refactoring

--- lib/20170418213954_change_date_and_time_column_name_fix.rb
+++ lib/20170418213954_change_date_and_time_column_name_fix.rb
@@ -1,5 +1,10 @@
 class ChangeDateAndTimeColumnNameFix < ActiveRecord::Migration[5.0]
+# below commented out because migration already completed for dev
+# but prod doesnt need this migration
+
+=begin
   def change
     rename_column :concerts, :datandtime, :dateandtime
   end
+=end
 end
