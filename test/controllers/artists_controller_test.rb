1e419284c6cadf8877f2124eb0c8dea393f65b2f
Brett F <brettfox11@gmail.com>
2017-05-04 08:09:39 -0500

    Add sendgrid

--- test/controllers/artists_controller_test.rb
+++ test/controllers/artists_controller_test.rb
@@ -1,14 +1,18 @@
 require 'test_helper'
 
 class ArtistsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get artists_show_url
-    assert_response :success
+  def setup
+    @michaelJackson = artists(:michaelJackson)
+    @u2 = artists(:u2)
   end
 
   test "should get index" do
-    get artists_index_url
+    get artists_path
     assert_response :success
   end
 
+  test "should get show" do
+    get artist_path(@u2)
+    assert_response :success
+  end
 end
--- test/controllers/countries_controller_test.rb
+++ test/controllers/countries_controller_test.rb
@@ -1,14 +1,5 @@
 require 'test_helper'
 
 class CountriesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get countries_show_url
-    assert_response :success
-  end
-
-  test "should get index" do
-    get countries_index_url
-    assert_response :success
-  end
 
 end
