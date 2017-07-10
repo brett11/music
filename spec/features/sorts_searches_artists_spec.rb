ce9d61aa67cb3daaea6d5d0f0ff9bcf67dd9a66d
Brett F <brettfox11@gmail.com>
2017-07-10 10:39:51 -0500

    Add rspec tests albums concerts

--- spec/features/sorts_searches_artists_spec.rb
+++ spec/features/sorts_searches_artists_spec.rb
@@ -2,8 +2,8 @@ require 'pry'
 require 'rails_helper'
 require_relative '../support/appear_before_matcher'
 
-RSpec.describe "sorts and searches artists" do
-    #can't use below because lazily loaded and need for once visit artists_path
+RSpec.describe "artists" do
+    #can't use below because lazily loaded and need our data right away for visit artists_path
     # let(:frank_ocean) { FactoryGirl.create(:artist, name_stage: "Frank Ocean", avatar: Rack::Test::UploadedFile.new("#{Rails.root}/spec/support/files/FrankOcean.jpg", 'image/jpeg') ) }
     # let(:u2) { FactoryGirl.create(:artist, name_stage: "U2", avatar: Rack::Test::UploadedFile.new("#{Rails.root}/spec/support/files/U2.jpg", 'image/jpeg') ) }
     # let(:bon_iver) { FactoryGirl.create(:artist, name_stage: "Bon Iver", avatar: Rack::Test::UploadedFile.new("#{Rails.root}/spec/support/files/BonIver.jpg", 'image/jpeg') ) }
@@ -14,7 +14,7 @@ RSpec.describe "sorts and searches artists" do
   end
 
 
-  it "sorts artists alphabetically" do
+  it "sorts by date" do
     visit artists_path
     #http://stackoverflow.com/questions/5228371/how-to-get-current-path-with-query-string-using-capybara
     # binding.pry
@@ -31,12 +31,12 @@ RSpec.describe "sorts and searches artists" do
     expect(@frank_ocean.name_stage).to appear_before(@bon_iver.name_stage)
   end
 
-  it "searches artists" do
+  it "searches" do
     visit artists_path
     expect(page).to have_selector("#frank_ocean")
     expect(page).to have_selector("#u2")
     expect(page).to have_selector("#bon_iver")
-    fill_in "search", with: "Frank"
+    fill_in "search", with: "frank"
     click_button "Search"
     expect(page).to have_selector("#frank_ocean")
     expect(page).to_not have_selector("#u2")
---
+++ spec/features/sorts_searches_concerts_spec.rb
@@ -0,0 +1,51 @@
+require 'pry'
+require 'rails_helper'
+require_relative '../support/appear_before_matcher'
+
+RSpec.describe "concerts" do
+  #can't use "let" stmts because lazily loaded and need our data right away for visit concerts_path
+
+  before(:example) do
+    @frank_ocean = FactoryGirl.create(:artist, name_stage: "Frank Ocean", avatar: Rack::Test::UploadedFile.new("#{Rails.root}/spec/support/files/FrankOcean.jpg", 'image/jpeg') )
+    @u2 = FactoryGirl.create(:artist, name_stage: "U2", avatar: Rack::Test::UploadedFile.new("#{Rails.root}/spec/support/files/U2.jpg", 'image/jpeg') )
+    @bon_iver = FactoryGirl.create(:artist, name_stage: "Bon Iver", avatar: Rack::Test::UploadedFile.new("#{Rails.root}/spec/support/files/BonIver.jpg", 'image/jpeg') )
+    @venue = FactoryGirl.create(:venue)
+    @frank_ocean_concert_2012 = FactoryGirl.create(:concert, dateandtime: "2018-07-10", artists: [@frank_ocean], venue: @venue )
+    @u2_concert_1987 = FactoryGirl.create(:concert, dateandtime: "2018-07-09", artists: [@u2], venue: @venue )
+    @bon_iver_concert_2007 = FactoryGirl.create(:concert, dateandtime: "2018-07-08", artists: [@bon_iver], venue: @venue )
+  end
+
+
+  it "sorts concerts by date and time" do
+    visit concerts_path
+    #http://stackoverflow.com/questions/5228371/how-to-get-current-path-with-query-string-using-capybara
+    expect(page).to have_selector("#frank_ocean_the_venue_20180710")
+    expect(page).to have_selector("#u2_the_venue_20180709")
+    expect(page).to have_selector("#bon_iver_the_venue_20180708")
+    #note that default of concerts index is to be sorted by most recent date.
+    #note that default of aritsts index is to be sorted alphabetically.
+    expect(@bon_iver_concert_2007.artists[0].name_stage).to appear_before(@u2_concert_1987.artists[0].name_stage)
+    expect(@u2_concert_1987.artists[0].name_stage).to appear_before(@frank_ocean_concert_2012.artists[0].name_stage)
+  end
+
+  it "sorts concerts by artist(alphabetically)" do
+    visit concerts_path
+    #http://stackoverflow.com/questions/5228371/how-to-get-current-path-with-query-string-using-capybara
+    expect(page).to have_selector("#frank_ocean_the_venue_20180710")
+    expect(page).to have_selector("#u2_the_venue_20180709")
+    expect(page).to have_selector("#bon_iver_the_venue_20180708")
+    #note that default of concerts index is to be sorted by most recent date.
+  end
+
+  it "searches concerts" do
+    visit concerts_path
+    expect(page).to have_selector("#frank_ocean_the_venue_20180710")
+    expect(page).to have_selector("#u2_the_venue_20180709")
+    expect(page).to have_selector("#bon_iver_the_venue_20180708")
+    fill_in "search", with: "frank"
+    click_button "Search"
+    expect(page).to have_selector("#frank_ocean_the_venue_20180710")
+    expect(page).to_not have_selector("#u2_the_venue_19870309")
+    expect(page).to_not have_selector("#bon_iver_the_venue_20180708")
+  end
+end
