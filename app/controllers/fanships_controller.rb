c4183aef14285852d70c30e06263fb047af58960
Brett F <brettfox11@gmail.com>
2017-07-12 14:32:10 -0500

    Change redirect action on follow and unfollows

--- app/controllers/fanships_controller.rb
+++ app/controllers/fanships_controller.rb
@@ -4,12 +4,13 @@ class FanshipsController < ApplicationController
   def create
     artist = Artist.find(params[:artist_id])
     current_user.follow(artist)
-    redirect_to current_user
+    # https://stackoverflow.com/questions/7465259/how-can-i-reload-the-current-page-in-ruby-on-rails
+    redirect_back(fallback_location: current_user)
   end
 
   def destroy
     artist = Fanship.find(params[:id]).artist
     current_user.unfollow(artist)
-    redirect_to artist
+    redirect_back(fallback_location: current_user)
   end
 end
