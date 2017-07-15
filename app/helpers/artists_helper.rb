042bf0692431c84d005c71bdbd4952fc06d13d22
Brett F <brettfox11@gmail.com>
2017-07-15 08:17:42 -0500

    Artists index search sort based on my favs changes

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,6 +4,6 @@ module ArtistsHelper
     title ||= column.titleize
     css_class = column == sort_column(column_table) && column_table == sort_table ? "current #{sort_direction}" : nil
     direction = column == sort_column(column_table) && sort_direction == "asc" ? "desc" : "asc"
-    button_to title, "#{current_route}", { method: :get, params: request.query_parameters.merge({sort_table: column_table, sort: column, direction: direction, page: 1, sort_favs: false}), class: css_class}
+    button_to title, "#{current_route}", { method: :get, params: request.query_parameters.merge({sort_table: column_table, sort: column, direction: direction, page: 1}), class: css_class}
   end
 end
